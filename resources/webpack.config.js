3754b11ec3088d578eeee7414d5a280741b787dc
Asko Soukka <asko.soukka@iki.fi>
2017-06-13 15:16:08 +0300

    Use current formunloadalert

--- resources/webpack.config.js
+++ resources/webpack.config.js
@@ -51,8 +51,8 @@ const common = {
     alias: {
       'mosaic': path.join(PATHS.mosaic, 'mosaic.pattern'),
       'mosaic-url': path.join(PATHS.mosaic),
-      'mockup-patterns-inlinevalidation': path.join(
-        PATHS.src, 'extensions', 'mockup-patterns-inlinevalidation'),
+      'mockup-patterns-inlinevalidation': path.join(PATHS.src, 'extensions', 'mockup-patterns-inlinevalidation'),
+      'mockup-patterns-formunloadalert': path.join(PATHS.mockup, 'formunloadalert', 'pattern'),
       'mockup-patterns-relateditems': path.join(PATHS.mockup, 'relateditems', 'pattern'),
       'mockup-patterns-relateditems-url': path.join(PATHS.mockup, 'relateditems'),
       'mockup-patterns-relateditems-upload': path.join(PATHS.mockup, 'relateditems', 'upload')
